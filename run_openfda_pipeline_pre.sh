b259b732102440f7617ef0be37b82e62a5d5d33b
Miguel C <mkarmona@gmail.com>
2019-09-17 17:50:03 +0100

    fix: move to json lines as multiline needs to be just iterate each file

--- run_openfda_pipeline_pre.sh
+++ run_openfda_pipeline_pre.sh
@@ -5,6 +5,6 @@ java -Dspark.master=local[*] \
   -Dspark.executor.heartbeatInterval=60000 \
   -Dspark.sql.crossJoin.enabled=true \
   -Dspark.driver.maxResultSize=0 \
-  -Dspark.executor.memory=6g \
-  -Dspark.driver.memory=6g \
+  -Dspark.executor.memory=2g \
+  -Dspark.driver.memory=2g \
   -cp metorikku-standalone.jar com.yotpo.metorikku.Metorikku -c openfda_pre.yaml
