7515b5593d49e2cd7cb1772668bc48176f6f0b33
Miguel C <mkarmona@gmail.com>
2019-09-17 13:29:49 +0100

    fix: flatten fix and simplification applying field extraction earlier

--- run_openfda_pipeline.sh
+++ run_openfda_pipeline.sh
@@ -1,2 +1,10 @@
-ulimit -c unlimited
-java -Dspark.master=local[*] -Dspark.sql.broadcastTimeout=60000 -Dspark.executor.heartbeatInterval=60000 -Dspark.sql.crossJoin.enabled=true -Dspark.driver.maxResultSize=0 -cp metorikku-standalone.jar com.yotpo.metorikku.Metorikku -c openfda.yaml
+#!/bin/bash
+
+java -Dspark.master=local[*] \
+  -Dspark.sql.broadcastTimeout=60000 \
+  -Dspark.executor.heartbeatInterval=60000 \
+  -Dspark.sql.crossJoin.enabled=true \
+  -Dspark.driver.maxResultSize=0 \
+  -Dspark.executor.memory=16g \
+  -Dspark.driver.memory=32g \
+  -cp metorikku-standalone.jar com.yotpo.metorikku.Metorikku -c openfda.yaml
