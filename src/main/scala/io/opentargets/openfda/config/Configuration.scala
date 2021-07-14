edbd524fd6a6405a7e0cd3365554188bba4b6e91
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-07-14 10:44:43 +0100

    code beautifying

--- src/main/scala/io/opentargets/openfda/config/Configuration.scala
+++ src/main/scala/io/opentargets/openfda/config/Configuration.scala
@@ -6,32 +6,39 @@ import pureconfig.ConfigSource
 import pureconfig.generic.auto._
 
 case class OTConfig(sparkUri: Option[String], common: Common, fda: Fda)
+
 case class Common(defaultSteps: Seq[String], output: String)
+
 case class FdaInputs(
-    blacklist: String,
-    chemblData: String,
-    fdaData: String,
-    meddraRelease: Option[String]
-) {
+                      blacklist: String,
+                      chemblData: String,
+                      fdaData: String,
+                      meddraRelease: Option[String]
+                    ) {
   require(blacklist.endsWith("txt"), "Blacklist is required in txt extension")
   require(chemblData.endsWith("json"), "ChEMBL data is required in json format")
   require(fdaData.endsWith("json") || fdaData.endsWith("jsonl"),
-          "FDA data is required in jsonl format")
+    "FDA data is required in jsonl format")
 }
+
 case class Sampling(output: String, size: Double = 0.1, enabled: Boolean = false) {
   require(size > 0)
 }
+
 case class MonteCarlo(permutations: Int, percentile: Double) {
   require(permutations > 0)
   require(percentile > 0 && percentile < 1)
 }
+
 case class Fda(montecarlo: MonteCarlo,
                fdaInputs: FdaInputs,
                outputs: Seq[String],
                sampling: Sampling) {
-  private def validOutput(str: String): Boolean = List("csv", "json", "jsonl","parquet").contains(str)
+  private def validOutput(str: String): Boolean = List("csv", "json", "jsonl", "parquet").contains(str)
+
   require(outputs.forall(validOutput))
 }
+
 object Configuration extends LazyLogging {
   lazy val config: OTConfig = load
 
